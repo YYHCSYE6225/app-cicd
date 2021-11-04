f9ced2da02dc9d2829671224803765d822745a79
YYhyang <44155580+YYhyang@users.noreply.github.com>
2021-11-04 13:16:11 -0400

    delete github actions

--- scripts/beforeInstall.sh
+++
@@ -1,2 +0,0 @@
-kill -9 $(netstat -nlp | grep :80 | awk '{print $7}' | awk -F"/" '{ print $1 }')
-sudo rm -rf /var/webapp
--- src/main/java/edu/neu/coe/csye6225/webapp/exception/UserExistException.java
+++
@@ -1,4 +0,0 @@
-package edu.neu.coe.csye6225.webapp.exception;
-
-public class UserExistException extends Exception{
-}
